9acca0a42c508d343536f75a3e72c5fa1a215de6
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-07-25 11:17:52 -0400

    Configured the project

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,23 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    // vitest config, with helpful vitest typing :)
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+  },
 })
