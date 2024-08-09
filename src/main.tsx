f943fc4bec33d33f5a303571260b1c9c06499f1a
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-08-09 05:05:14 -0400

    feat(router) added router(doesn't work yet)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App/App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import { addToCart } from './shoppingCartApi'
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <Root />,
+    children: [
+      {index: true, element: <Index />},
+      {
+        path: 'catalog/:sex',
+        element: <Catalog />,
+        loader: getCatalog
+      },
+      {
+        path: 'catalog/:sex/:itemId',
+        element: <Item />,
+        loader: getItem,
+        action: addToCart
+      },
+      {
+        path: 'cart',
+        element: loadTotal,
+        action: buy,
+        children: [
+          {
+            path: 'items',
+            element: <CartItem />,
+            loader: getCartItems,
+            action: cartAction,
+          },
+        ],
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
