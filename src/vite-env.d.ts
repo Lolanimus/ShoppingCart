664ee9ff7c046ea2b8591c003ef30d6dbc0a1290
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-08-10 06:36:28 -0400

    refactor: change the way the cart stores info, make tests more convient
    Now, instead of json file, api will use a module with its getters and setters.
    Also, introduced mocked module data for tests, for convenience
    Some small changes to the inner working of the api was introduces, as well

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+declare type CatalogObj = { 
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    image: string,
+    rating: {
+        rate: number,
+        count: number,
+    },
+};
+declare type CartObj = CatalogObj & { quantity: number, size?: string };
+declare type CatalogArr = CatalogObj[];
+declare type CartArr = CartObj[];
