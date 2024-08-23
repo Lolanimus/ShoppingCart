d0e730bb70c36a9700ab8ebc349494b653ad1d0c
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-08-23 07:25:32 -0400

    test: refactored e2e tests
    I honetsly have no idea what I was trying to test before this commit,
    so I fixed it. Now I think it makes sense what it tests.

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -13,7 +13,7 @@ test.describe("Root", () => {
 
     const menCatalog = await page.getByRole("link", { name: "Men", exact: true }).all();
     await menCatalog[0].click()
-    await page.waitForURL(baseURL + "/catalog/men");
+    await expect(page.getByRole("heading", {level: 1, name: "• Men"})).toBeInViewport();
   })
 
   test("goes to women's catalog", async ({ page, baseURL }) => {
@@ -21,7 +21,7 @@ test.describe("Root", () => {
 
     const womenCatalog = await page.getByRole("link", { name: "Women", exact: true }).all();
     await womenCatalog[0].click()
-    await page.waitForURL(baseURL + "/catalog/women");
+    await expect(page.getByRole("heading", {level: 1, name: "• Women"})).toBeInViewport();
   })
 
   test("goes to cart", async ({ page, baseURL }) => {
@@ -29,15 +29,15 @@ test.describe("Root", () => {
 
     const cart = page.getByRole("link", { name: "Cart Image" });
     await cart.click()
-    await page.waitForURL(baseURL + "/cart");
+    await expect(page.getByRole("heading", {level: 1, name: "Cart"})).toBeInViewport();
   })
   
   test("goes to main page", async ({ page, baseURL }) => {
     await page.goto(baseURL + '/cart');
     expect(await page.getByRole("heading", {level: 1}).textContent()).toBe("Cart");
-    const logo = page.getByRole("img", { name: "Lolan Logo" }).locator("..");
+    const logo = page.getByRole("img", { name: "Lolan Logo" });
     await logo.click();
-    await page.waitForURL(baseURL!);
+    await expect(page.getByRole("heading", {level: 1, name: "Lolan"})).toBeInViewport();
   })
 })
 
@@ -47,7 +47,7 @@ test.describe("Index", () => {
 
     const menCatalog = await page.getByRole("link", { name: "Men", exact: true }).all();
     await menCatalog[1].click()
-    await page.waitForURL(baseURL + "/catalog/men");
+    await expect(page.getByRole("heading", {level: 1, name: "• Men"})).toBeInViewport();
   })
 
   test("goes to women's catalog", async ({ page, baseURL }) => {
@@ -55,26 +55,30 @@ test.describe("Index", () => {
 
     const womenCatalog = await page.getByRole("link", { name: "Women", exact: true }).all();
     await womenCatalog[1].click()
-    await page.waitForURL(baseURL + "/catalog/women");
+    await expect(page.getByRole("heading", {level: 1, name: "• Women"})).toBeInViewport();
   })
 })
 
 test.describe("Catalog", () => {
   test("goes to a catalogItem when See More btn is clicked", async ({ page, baseURL }) => {
     await page.goto(baseURL + "/catalog/men");
+    expect(page.getByRole("heading", {level: 1, name: "• Men"}));
+    expect(page.locator("#item1"));
     const firstForm = page.locator("#item1");
-    await expect(firstForm).toBeAttached();
     const seeMoreBtn = firstForm.locator("button");
     await seeMoreBtn.click();
-    await page.waitForURL(baseURL + "/catalog/men/1");
+    await expect(page.getByRole("button", {name: "Add to Cart"})).toBeInViewport();
   })
 })
 
 test.describe("CatalogItem", () => {
   test("user can choose a size, if approptiate", async ({ page, baseURL }) => {
     await page.goto(baseURL + "/catalog/men/1");
+    expect(page.getByRole("heading", {level: 1, name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"}));
     const m = page.getByLabel("M", { exact: true });
     const s = page.getByLabel("S", { exact: true });
+    expect(m);
+    expect(s);
     await expect(s).toBeChecked();
     await m.click();
     await expect(m).toBeEnabled();
@@ -83,11 +87,13 @@ test.describe("CatalogItem", () => {
     await expect(addToCartBtn).toBeDisabled();
   })
 
-  test("item with no size", async ({ page, baseURL }) => {
+  test.only("item with no size", async ({ page, baseURL }) => {
     await page.goto(baseURL + "/catalog/men/5");
     const oneSize = page.getByLabel("One size");
+    expect(oneSize);
     await expect(oneSize).toBeChecked();
     const addToCartBtn = page.getByRole("button", {name: "Add to Cart"});
+    expect(addToCartBtn);
     await addToCartBtn.click();
     await expect(addToCartBtn).toBeDisabled();
   })
